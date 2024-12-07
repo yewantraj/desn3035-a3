850d9c2549c0059009bdadbd40c8a9f830f8f18f
Yewantraj Karki <yewantarajkarki00@gmail.com>
2024-12-07 06:03:52 +0000

    Routes has been added with required file are imported

--- src/App.js
+++ src/App.js
@@ -8,9 +8,43 @@ import About from './components/About';
 import Team from './components/Team';
 import Contact from './components/Contact';
 import Service from './components/Service';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/assets/main.css';
+import { initializeCustomScripts } from './components/assets/main.js';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 function App() {
+  useEffect(() => {
+    const cleanup = initializeCustomScripts();
+    return cleanup;
+  }, []);
+
+  useEffect(() => {
+   
+    AOS.init({
+      duration: 1200, 
+      once: true,     
+    });
+
+    return () => {
+      AOS.refresh(); 
+    };
+  }, []);  useEffect(() => {
+   
+    AOS.init({
+      duration: 600,     
+      easing: 'ease-in-out', 
+      once: true,       
+      mirror: false,     
+    });
+
+ 
+    return () => {
+      AOS.refresh(); 
+    };
+  }, []); 
   return (
     <BrowserRouter>
       <Nav />
