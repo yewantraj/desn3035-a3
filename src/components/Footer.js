3911c340e8ee8231dadda87973794bbf221d305e
Yewantraj Karki <yewantarajkarki00@gmail.com>
2024-12-07 09:26:06 +0000

    navbar and footer design added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,82 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
 function Footer() {
   return (
     <>
-     
+      <footer id="footer" className="footer">
+        <div className="container footer-top">
+          <div className="row gy-4">
+            <div className="col-lg-4 col-md-6 footer-about">
+              <Link to="/" className="d-flex align-items-center">
+                <span className="sitename">sigma tech</span>
+              </Link>
+              <div className="footer-contact pt-3">
+                <p>A108 Adam Street</p>
+                <p>New York, NY 535022</p>
+                <p className="mt-3">
+                  <strong>Phone:</strong> <span>+1 5589 55488 55</span>
+                </p>
+                <p className="email">
+                  <strong>Email:</strong>
+                  <span>
+                    <a href="mailto:sigmatech@gmail.com">sigmatech@gmail.com</a>
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li>
+                  <i className="bi bi-chevron-right"></i> <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <Link to="/about">About us</Link>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <Link to="/service">Services</Link>
+                </li>
+                <li>
+                  <i className="bi bi-chevron-right"></i>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </div>
+
+           
+
+            <div className="col-lg-4 col-md-12">
+              <h4>Follow Us</h4>
+              <p>
+                Cras fermentum odio eu feugiat lide par naso tierra videa magna
+                derita valies
+              </p>
+              <div className="social-links d-flex">
+                <a target='_blank' href="https://www.facebook.com/">
+                  <i className="bi bi-facebook"></i>
+                </a>
+                <a target='_blank' href="https://www.instagram.com/">
+                  <i className="bi bi-instagram"></i>
+                </a>
+                <a target='_blank' href="https://www.linkedin.com/">
+                  <i className="bi bi-linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container copyright text-center mt-4">
+          <p>
+            © <span>Copyright</span>
+            <strong className="px-1 sitename">SigmaTech</strong>
+            <span>All Rights Reserved</span>
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,44 @@
+import React from 'react';
+import {Link, useLocation} from 'react-router-dom';
+
+
+
 function Nav() {
- 
+  const location = useLocation();
   return (
     <>
-     
+      <header id="header" className="header d-flex align-items-center fixed-top">
+        <div className="container-fluid container-xl position-relative d-flex align-items-center">
+          <Link to="/" className="logo d-flex align-items-center me-auto">
+            <h1 className="sitename">SigmaTech</h1>
+          </Link>
+
+          <nav id="navmenu" className="navmenu">
+            <ul>
+              <li>
+                <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link>
+              </li>
+              <li>
+                <Link to="/service" className={location.pathname === '/service' ? 'active' : ''} >Services</Link>
+              </li>
+              <li>
+                <Link to="/team" className={location.pathname === '/team' ? 'active' : ''}>Team</Link>
+              </li>
+              
+            </ul>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
+
+          <Link className="btn-getstarted" to="/contact">
+            Contact
+          </Link>
+        </div>
+      </header>
     </>
   );
 }
